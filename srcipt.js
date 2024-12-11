4a4664e76b52ad14dae198103a7e51214fa1afd8
Pilter Barnabás <pilter.barnabas.jozsef@students.jedlik.eu>
2024-12-11 10:01:21 +0100

    js javítva

--- srcipt.js
+++ srcipt.js
@@ -1,6 +1,8 @@
 const jelek = ["karo", "treff", "pikk", "tök"]
 const szamok = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "B", "D", "K", "A"]
 
+const kihuzott = []
+
 function emeles(x){
     const tet = document.getElementById('tet')
     const penz = document.getElementById('penz')
@@ -18,10 +20,24 @@ function emeles(x){
 
 }
 
+var clicks = 2
 function hit(){
+    clicks += 1
+    console.log(clicks)
     const jel = jelek[Math.floor(Math.random()*jelek.length)]
     console.log(jel)
     const szam = szamok[Math.floor(Math.random()*szamok.length)]
     console.log(szam)
     
+}
+
+
+ function double(){
+     const tet = document.getElementById('tet')
+     const penz = document.getElementById('penz')
+     parseInt(tet.innerText) * 2
+     if (parseInt(tet.innerText) > parseInt(penz.innerText)){
+         alert("Nem emelhetsz többet!")
+         tet.innerText = parseInt(penz.innerText)
+     }
 }
